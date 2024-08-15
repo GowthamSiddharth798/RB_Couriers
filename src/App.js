3e02d93c8af7b1bd652809fd6e60c10b9875a3a0
gowtham <gowthamsiddharth75@gmail.com>
2024-08-15 15:42:20 +0530

    Fixed unused Carousel import issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Linker from './Linker.js'
-// import Lin
 function App() {
   return (
     <>
